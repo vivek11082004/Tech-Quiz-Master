76607c49139ecad05491289b0e146ee9ea026ac3
Vivek_dev <vivek_227043@saitm.org>
2026-05-02 00:10:41 +0530

    fix: use env variable for MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://vivekbhai100vk_db_user:GmYid5mKMMofrJga@cluster0.z3dnyru.mongodb.net/TechQuiz").then(() => {
+    await mongoose.connect(process.env.MONGODB_URI).then(() => {
         console.log("Connected to MongoDB");
     })
 }
